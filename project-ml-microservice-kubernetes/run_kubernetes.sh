a49419bd16276e2edc64455bb660e0ecbe382ba6
Tung Bach <thanhtung249@gmail.com>
2022-12-15 21:20:08 +0700

    add p4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=thanhtung249/udacity_p4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity\
+    --image=$dockerpath 
+    --port=80 --labels app=udacity
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity 8000:80
